35a4d9974573659196cfac413f6856b71f3dea61
Wikwoj0512 <35971049+Wikwoj0512@users.noreply.github.com>
2020-07-20 09:29:22 +0200

    Update createService.bat

--- createService.bat
+++ createService.bat
@@ -3,7 +3,7 @@ set python_path=%~dp0Python\python.exe
 set service_path=%~dp0service.py
 set working_directory=%~dp0
 
-"%pythonpath%" -m pip install -U -r requirements.txt
+"%python_path%" -m pip install -U -r requirements.txt
 
 sc create MockerService binPath= "%python_path% %service_path% %working_directory%"
 
